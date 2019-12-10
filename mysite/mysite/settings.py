b4e615afe861fb4d11cc079685b1e937bedeafe4
Angelium <salman666online@gmail.com>
2019-12-10 16:20:46 +0500

    login

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +127,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username is Django admin, regardless of 'allauth'
+    'django.contrib.auth.backends.ModelBackend',
+
+    # 'allauth' specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+LOGIN_REDIRECT_URL = '/'
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,6 +10,19 @@ from rest_framework import viewsets
 
 from .models import Choice, Question
 
+class LoginView(generic.ListView):
+    template_name = 'polls/login.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
